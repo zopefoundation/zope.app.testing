52f56dea1e256822dfacadc654d10ff9a76a9b72
Stephan Richter <stephan.richter@gmail.com>
2005-01-14 22:31:22 +0000

    yipee, pluggable auth is already fixed, at least the unit tests are.

--- setup.py
+++ setup.py
@@ -21,6 +21,32 @@ from zope.app import zapi
 from zope.app.testing import ztapi
 from zope.interface import classImplements
 
+
+#############################################################################
+# BBB: Goes away in 3.3
+
+from zope.component.bbb.service import IService
+from zope.app.site.interfaces import ISimpleService
+from zope.app.component.site import UtilityRegistration
+
+def addService(servicemanager, name, service, suffix=''):
+    """Add a service to a service manager
+
+    This utility is useful for tests that need to set up services.
+    """
+    # Most local services implement ISimpleService in ZCML; therefore make
+    # sure we got it here as well.
+    zope.interface.directlyProvides(service, ISimpleService)
+
+    default = zapi.traverse(servicemanager, 'default')
+    default[name+suffix] = service
+    registration = UtilityRegistration(name, IService, service, default)
+    key = default.registrationManager.addRegistration(registration)
+    zapi.traverse(default.registrationManager, key).status = ActiveStatus
+    return default[name+suffix]
+
+#############################################################################
+
 #------------------------------------------------------------------------
 # Annotations
 from zope.app.annotation.attribute import AttributeAnnotations
