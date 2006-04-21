c6cdf265351121b922dcee3e8f187fa8c353656d
Jim Fulton <jim@zope.com>
2006-04-21 16:37:24 +0000

    Services are gone

--- back35.py
+++ back35.py
@@ -19,7 +19,6 @@ $Id$
 from zope.app import zapi
 import zope.interface
 from zope.component.service import IService
-from zope.app.site.interfaces import ISimpleService
 from zope.app.component.site import UtilityRegistration
 from zope.app.component.back35 import ActiveStatus
 
@@ -28,10 +27,6 @@ def addService(servicemanager, name, service, suffix=''):
 
     This utility is useful for tests that need to set up services.
     """
-    # Most local services implement ISimpleService in ZCML; therefore make
-    # sure we got it here as well.
-    zope.interface.directlyProvides(service, ISimpleService)
-
     default = zapi.traverse(servicemanager, 'default')
     default[name+suffix] = service
     registration = UtilityRegistration(name, IService, service, default)
