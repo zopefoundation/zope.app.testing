afa0c16307e29c42a14b23cdd85317ff08c8ecf3
Martijn Pieters <mj@zopatista.co>
2006-08-01 16:05:25 +0000

    Cookie paths are always set with the lowercase "path"; this code never works otherwise

--- functional.py
+++ functional.py
@@ -278,8 +278,8 @@ class CookieHandler(object):
         for k,v in response._cookies.items():
             k = k.encode('utf8')
             self.cookies[k] = v['value'].encode('utf8')
-            if self.cookies[k].has_key('Path'):
-                self.cookies[k]['Path'] = v['Path']
+            if self.cookies[k].has_key('path'):
+                self.cookies[k]['path'] = v['path']
 
 
 class BrowserTestCase(CookieHandler, FunctionalTestCase):
