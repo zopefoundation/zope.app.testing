e30342274c19c08ae90b28eb67748cd5f59ff837
Bernd Dorn <bernddorn@gmail.com>
2007-03-25 10:57:08 +0000

    fix zcmlfiles import

--- tests.py
+++ tests.py
@@ -237,19 +237,20 @@ def test_include_of_zope_app():
     >>> from zope.configuration import xmlconfig, config
     >>> context = config.ConfigurationMachine()
     >>> xmlconfig.registerCommonDirectives(context)
-    >>> import zope.app
+    >>> import zope.app.zcmlfiles
 
     >>> import warnings
     >>> showwarning = warnings.showwarning
     >>> warnings.showwarning = lambda *a, **k: None
 
-    >>> xmlconfig.include(context, package=zope.app)
+    >>> xmlconfig.include(context, package=zope.app.zcmlfiles)
 
-    >>> xmlconfig.include(context, 'configure.zcml', zope.app)
-    >>> xmlconfig.include(context, 'ftesting.zcml', zope.app)
-    >>> xmlconfig.include(context, 'menus.zcml', zope.app)
-    >>> xmlconfig.include(context, 'meta.zcml', zope.app)
-    >>> xmlconfig.include(context, 'file_not_exists.zcml', zope.app) #doctest: +ELLIPSIS
+    >>> xmlconfig.include(context, 'configure.zcml', zope.app.zcmlfiles)
+    >>> xmlconfig.include(context, 'ftesting.zcml', zope.app.zcmlfiles)
+    >>> xmlconfig.include(context, 'menus.zcml', zope.app.zcmlfiles)
+    >>> xmlconfig.include(context, 'meta.zcml', zope.app.zcmlfiles)
+    >>> xmlconfig.include(context,
+    ...     'file_not_exists.zcml', zope.app.zcmlfiles) #doctest: +ELLIPSIS
     Traceback (most recent call last):
     ...
     IOError: ...
