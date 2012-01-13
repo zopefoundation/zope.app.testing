a79be6da5a3098c6f5d2b8dcf2f2d829020583a8
Jim Fulton <jim@zope.com>
2012-01-13 23:19:15 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.testing',
-      version = '3.9.1dev',
+      version = '3.10.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Application Testing Support',
