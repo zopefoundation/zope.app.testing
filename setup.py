8ed8b1120c0becd153119e2054444e84e96454f3
Jason Madden <jamadden@gmail.com>
2017-12-19 08:55:45 -0600

    Move the dependency on zope.testbrowser into an extra.
    And pin it to a version that works with this package.

--- setup.py
+++ setup.py
@@ -18,16 +18,23 @@
 ##############################################################################
 """Setup for zope.app.testing package
 
-$Id$
+
 """
 import os
 from setuptools import setup, find_packages
 
 def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+TESTBROWSER_REQUIRES = [
+    # zope.testbrowser version 5 has a new API and
+    # doesn't have the 'connection' module
+    'zope.testbrowser >=4,<5',
+]
 
 setup(name='zope.app.testing',
-      version = '3.9.1dev',
+      version='3.11.0.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Application Testing Support',
@@ -40,50 +47,59 @@ setup(name='zope.app.testing',
           + '\n\n' +
           read('CHANGES.txt')
           ),
-      keywords = "zope3 test testing setup functional",
-      classifiers = [
+      keywords="zope3 test testing setup functional",
+      classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Web Environment',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Zope Public License',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
           'Natural Language :: English',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
-          'Framework :: Zope3'],
-      url='http://pypi.python.org/pypi/zope.app.testing',
+          'Framework :: Zope3',
+      ],
+      url="https://github.com/zopefoundation/zope.app.testing",
       license='ZPL 2.1',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['zope', 'zope.app'],
-      extras_require=dict(test=[
-          'ZODB3',
-          'zope.app.zcmlfiles',
-          'zope.login',
-          'zope.publisher >= 3.12',
-          'zope.securitypolicy',
-          ]),
-      install_requires=['setuptools',
-                        'zope.annotation',
-                        'zope.app.appsetup >= 3.11',
-                        'zope.processlifetime',
-                        'zope.app.debug',
-                        'zope.app.dependable',
-                        'zope.app.publication',
-                        # We need zope.component with the hooks module.
-                        'zope.component >= 3.8',
-                        'zope.container',
-                        'zope.i18n',
-                        'zope.interface',
-                        'zope.password',
-                        'zope.publisher',
-                        'zope.schema',
-                        'zope.security',
-                        'zope.site',
-                        'zope.testing',
-                        'zope.testbrowser >= 4',
-                        'zope.traversing',
-                        ],
-      include_package_data = True,
-      zip_safe = False,
-      )
+      extras_require={
+          'test': [
+              'ZODB',
+              'zope.app.zcmlfiles',
+              'zope.login',
+              'zope.publisher >= 3.12',
+              'zope.securitypolicy',
+              'zope.testrunner',
+          ] + TESTBROWSER_REQUIRES,
+          'testbrowser': TESTBROWSER_REQUIRES,
+      },
+      install_requires=[
+          'setuptools',
+          'zope.annotation',
+          'zope.app.appsetup >= 3.11',
+          'zope.processlifetime',
+          'zope.app.debug',
+          'zope.app.dependable',
+          'zope.app.publication',
+          # We need zope.component with the hooks module.
+          'zope.component >= 3.8',
+          'zope.container',
+          'zope.i18n >= 4.3.0',
+          'zope.interface',
+          'zope.password',
+          'zope.publisher',
+          'zope.schema',
+          'zope.security',
+          'zope.site',
+          'zope.testing',
+          'zope.traversing',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+)
